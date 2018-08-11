902988d06ddafaf2b1bbe8deaa4e0e3d7032fc5e
Putheng <puthengemail@gmail.com>
2018-08-11 14:45:34 +0700

    add HHVM to server

--- boot.sh
+++ boot.sh
@@ -18,6 +18,9 @@ sudo apt-get -y install unzip zip nginx
 # Install PHP 7.2 FPM
 sudo apt-get -y install php7.2 php7.2-fpm php7.2-mysql php7.2-mbstring php7.2-xml php7.2-curl
 
+# Install PHP 7.1 FPM
+sudo apt-get -y install php7.1 php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-mysql php7.1-mbstring php7.1-mcrypt php7.1-zip php7.1-fpm
+
 # Install PHP 7.0 FPM
 sudo apt-get -y install php7.0 php7.0-fpm php7.0-mysql php7.0-mbstring php7.0-xml php7.0-curl
 
@@ -26,6 +29,7 @@ sudo apt-get -y install php5.6 php5.6-fpm
 
 # Restart PHP
 sudo service php7.2-fpm restart
+sudo service php7.1-fpm restart
 sudo service php7.0-fpm restart
 sudo service php5.6-fpm restart
 
@@ -39,13 +43,33 @@ sudo apt-get install -y mysql-server
 # Install Composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
+## Setup HHVM
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+
+# HHVM's repository
+sudo add-apt-repository "deb http://dl.hhvm.com/ubuntu $(lsb_release -sc) main"
+
+# Update
+sudo apt-get update
+
+# Install HHVM
+sudo apt-get install hhvm
+
+#script which makes the integration with Nginx
+sudo /usr/share/hhvm/install_fastcgi.sh
+
+#=== Config Nginx to know HHVM
+# Change on Nginx sites enabled ==> fastcgi_pass   127.0.0.1:9000;
+
+
+
 # Remove default Nginx host
 rm -f /etc/nginx/sites-enabled/default
 
 # Create default host
-sudo mkdir /var/www
-sudo mkdir /var/www/html
-sudo mkdir /var/www/html/default
+sudo mkdir -p /var/www
+sudo mkdir -p /var/www/html
+sudo mkdir -p /var/www/html/default
 
 echo "<?php phpinfo(); ?>" > /var/www/html/default/index.php
 
